534369f9640305bc960500af29f07d53c1464cb0
VinayPathania <you@example.comvinaypathania2002@gmail.com>
2023-02-05 16:47:46 +0530

    Packages added

---
+++ foodinminutes/src/main/java/com/foodinminutes/FoodinminutesApplication.java
@@ -0,0 +1,13 @@
+package com.foodinminutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodinminutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodinminutesApplication.class, args);
+	}
+
+}
---
+++ foodinminutes/src/test/java/com/foodinminutes/foodinminutes/FoodinminutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodinminutes.foodinminutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodinminutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
